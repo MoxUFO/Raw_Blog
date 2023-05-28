003d593f8c1a4732a5f2dab43144ccce2907a90d
MOX UFO <mox.ufo@gmail.com>
2023-05-28 15:21:52 -0400

    dashboard now shows each post in general

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,14 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+const commentRoutes = require('./comments');
+const postRoutes = require('./post');
 
 router.use('/', homeRoutes);
-router.use('/comment', homeRoutes);
-router.use('/dashboard', homeRoutes);
-// router.use('/Homepage', homeRoutes);
+// router.use('/comment', commentRoutes);
+// router.use('/post', postRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
